4be68d5f16cd7bd53cba277185830d04c9f8bffd
Carlos Velasquez <carlosrobertovelasquez@gmail.com>
2020-08-24 22:35:51 -0600

    Se configura para que pueda ver css y scss

--- next.config.js
+++ next.config.js
@@ -1,24 +1,27 @@
 const withCSS = require('@zeit/next-css');
+const withSass = require('@zeit/next-sass');
 
-module.exports = withCSS({
-	target: 'serverless',
-	webpack(config) {
-		// Fixes npm packages that depend on `fs` module
-		config.node = {
-			fs: 'empty'
-		};
-		config.module.rules.push({
-			test: /\.(jpg|png|svg|eot|otf|ttf|woff|woff2)$/,
-			use: {
-				loader: 'url-loader',
-				options: {
-					limit: 8192,
-					publicPath: '/imagenes/',
-					outputPath: 'static/',
-					name: '[name].[ext]'
+module.exports = withSass(
+	withCSS({
+		target: 'serverless',
+		webpack(config) {
+			// Fixes npm packages that depend on `fs` module
+			config.node = {
+				fs: 'empty'
+			};
+			config.module.rules.push({
+				test: /\.(jpg|png|svg|eot|otf|ttf|woff|woff2)$/,
+				use: {
+					loader: 'url-loader',
+					options: {
+						limit: 8192,
+						publicPath: '/imagenes/',
+						outputPath: 'static/',
+						name: '[name].[ext]'
+					}
 				}
-			}
-		});
-		return config;
-	}
-});
+			});
+			return config;
+		}
+	})
+);
